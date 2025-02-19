cedd13419dc9d5f714d4b2e10dba83921baf4d9c
Sam Lehman <github@samlehman.dev>
2025-02-19 03:51:58 -0500

    fix(parts.treefmt): re-enable main formatter & add formatters

--- parts/treefmt.nix
+++ parts/treefmt.nix
@@ -1,25 +1,97 @@
-{inputs, ...}: {
+{
+  inputs,
+  self,
+  ...
+}: {
   imports = [inputs.treefmt-nix.flakeModule];
-  perSystem = {
-    config,
-    lib,
-    pkgs,
-    ...
-  }: {
+  perSystem = {pkgs, ...}: {
     treefmt = {
-      flakeCheck = true;
+      enableDefaultExcludes = true;
+      package = pkgs.treefmt;
+
+      flakeCheck = false;
       flakeFormatter = true;
+
+      projectRoot = self;
+
       programs = {
-        alejandra.enable = true;
+        # --- JavaScript ---
+        # TODO: Enable on dir: `../packages/themes-jsonresume/**`
+        prettier.enable = false;
+
+        # --- JSON ---
+        formatjson5 = {
+          enable = false;
+          package = pkgs.formatjson5;
+          excludes = [];
+          includes = ["*.json5" "*.json"];
+          indent = 2;
+          noTrailingCommas = false;
+          oneElementLines = true;
+          sortArrays = false;
+        };
+        jsonfmt = {
+          enable = true;
+          package = pkgs.jsonfmt;
+          excludes = ["*.json5"];
+          includes = ["*.json"];
+        };
+
+        # --- Jsonnet ---
+        jsonnet-lint.enable = false;
+        jsonnetfmt.enable = false;
+
+        # --- Markdown ---
+        mdformat = {
+          enable = true;
+          package = pkgs.python3Packages.mdformat;
+          excludes = [];
+          # includes = ["docs/*.md" "README.md"];
+          includes = ["*.md"];
+          priority = 2;
+          settings.end-of-line = "keep";
+          settings.number = false;
+          settings.wrap = 120;
+        };
+
+        # Markdown code block preprocessor
+        mdsh = {
+          enable = true;
+          package = pkgs.mdsh;
+          excludes = [];
+          includes = ["*.md"];
+          priority = 1;
+        };
+
+        # --- Nickel ---
+        nickel.enable = false;
+
+        # --- Nix ---
+        alejandra = {
+          enable = true;
+          includes = ["*.nix"];
+          excludes = ["packages/*.nix"];
+        };
+        nixfmt.enable = false;
+        nixfmt-rfc-style = {
+          enable = true;
+          package = pkgs.nixfmt-rfc-style;
+          excludes = ["flake.nix"];
+          includes = ["packages/*.nix"];
+          priority = 1;
+        };
+        nixpkgs-fmt.enable = false;
         deadnix.enable = false;
         statix.enable = false;
-        nixfmt.enable = false;
-        nixpkgs-fmt.enable = false;
-        formatjson5.enable = false;
+
+        # --- Nushell ---
+        nufmt.enable = false;
+
+        # --- YAML ---
+        # GitHub Actions
+        actionlint.enable = false;
         yamlfmt.enable = true;
       };
-      projectRoot = inputs.self;
-      projectRootFile = "flake.nix";
     };
   };
 }
