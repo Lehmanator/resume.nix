0f6b2ba3c474c9e450e884e4f527698b6688160f
Sam Lehman <github@samlehman.dev>
2025-06-09 12:00:25 -0400

    refactor(flake): cleanup comments

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
     pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
     haumea.url = "github:nix-community/haumea";
     incl.url = "github:divnix/incl";
-    #std.url = "github:divnix/std";
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -29,11 +28,10 @@
       url = "github:Lehmanator/rendercv";
       flake = false;
     };
-    #"github:sinaatalay/rendercv";
+    # "github:sinaatalay/rendercv";
   };
   outputs = {
     self,
-    nixpkgs,
     flake-parts,
     haumea,
     ...
@@ -48,10 +46,7 @@
         ...
       }: {
         formatter = pkgs.alejandra;
-        #overlayAttrs = { inherit (packages) jsonresume-data; };
         apps = rec {
-          # TODO: Live server
-          # serve = { type = "app"; program = ""; };
           # TODO: Init resume, convert to nix
           init = {
             type = "app";
@@ -76,7 +71,7 @@
               ${lib.getExe pkgs.puppeteer-cli} screenshot \
                 ${config.packages.jsonresume-format-html}/index.html \
                 $outfile
-                #--viewport 1920x1080 \
+                # --viewport 1920x1080 \
             '';
           };
           pdf = {
@@ -154,7 +149,6 @@
           // jsonresume-formats
           // {
             inherit (pkgs) resumed puppeteer-cli;
-            # default = config.packages.resume;
             jsonresume-builder-pdf = pkgs.writeShellScript "resumed-render-pdf" ''
               ${lib.getExe pkgs.puppeteer-cli} print ${config.packages.jsonresume-format-html}/index.html ./jsonresume.pdf
             '';
@@ -174,28 +168,7 @@
                 inherit (project.renderers) buildPythonPackage;
                 python3 = pkgs.callPackage ./packages/rendercv/python.nix {};
               };
-            #resume-cli = pkgs.callPackage ./packages/resume-cli {};
-
-            # TODO: Wrap with webserver?
-
-            #themes-checks = let
-            #  builderAttrs = l.filterAttrs (n: _: l.hasPrefix "resume-" n) packages.${pkgs.system};
-            #in pkgs.stdenv.mkDerivation {
-            #  name = "themes-checks";
-            #  src = ./template;
-            #  buildPhase = ''
-            #    cp resume.sample.json resume.json
-            #  '' + (l.concatStringsSep "\n\n" (l.attrValues (l.mapAttrs (n: v: ''
-            #    # Build using builder ${n}
-            #    ${v}
-            #    mv resume.html ${n}.html
-            #  '') builderAttrs)));
-            #  installPhase = ''
-            #    mkdir $out
-            #  '' + (l.concatStringsSep "\n\n" (l.attrValues
-            #    (l.mapAttrs (n: _: "mv ${n}.html $out") builderAttrs)
-            #  ));
-            #};
+            # resume-cli = pkgs.callPackage ./packages/resume-cli {};
           };
       };
       flake = {
@@ -205,13 +178,8 @@
           transformer = haumea.lib.transformers.liftDefault;
           inputs = {
             lib = inputs.nixpkgs.lib;
-            # pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-            # inherit lib pkgs;
           };
         };
-
-        # templates.default = { path = ./template; description = "Template to build jsonresume with nix"; };
-        # checks.themes = self.packages.themes-checks; #  # Check output to run checks for all themes
       };
     };
 }
