3a717d25c5eb4f47ca52939a931b0c44b6a6e476
Sam Lehman <lehmanator@noreply.codeberg.org>
2025-06-22 16:19:51 -0400

    fix(packages.formats.yaml): temporarily? remove `$schema` attr [WIP]

--- packages/jsonresume/formats/yaml.nix
+++ packages/jsonresume/formats/yaml.nix
@@ -15,7 +15,7 @@
   version = args.version or data.meta.version or "v0.0.1";
 in
   ((formats.yaml {}).generate "jsonresume-${theme}-${basename}.yaml" (lib.recursiveUpdate data {
-    "$schema" = "https://raw.githubusercontent.com/jsonresume/resume-schema/v1.0.0/schema.json";
+    # "$schema" = "https://raw.githubusercontent.com/jsonresume/resume-schema/v1.0.0/schema.json";
     meta.canonical = "https://raw.githubusercontent.com/Lehmanator/resume.nix/main/src/jsonresume/${basename}.nix";
     meta.theme = theme;
     meta.version = version;
