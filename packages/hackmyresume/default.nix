0099306b02908c6cc36557ef8f5bf0f6d7b01791
Sam Lehman <lehmanator@noreply.codeberg.org>
2025-06-22 17:09:26 -0400

    refactor(packages.hackmyresume): formatting, comments, & add license to metadata.

--- packages/hackmyresume/default.nix
+++ packages/hackmyresume/default.nix
@@ -2,46 +2,34 @@
   buildNpmPackage,
   fetchFromGitHub,
   lib,
-}: let
+  ...
+}:
+# https://github.com/hacksalot/HackMyResume
+buildNpmPackage rec {
   pname = "HackMyResume";
-  version = "v1.9.1";
-  #
-  # https://nixos.org/manual/nixpkgs/unstable/#language-javascript
-  #
-  # Builders:
-  # - buildNpmPackage
-  # - node2nix
-  # - yarn2nix
-  # - mkYarnPackage
-  # - mkYarnModules
-  # - npmlock2nix (https://github.com/nix-community/npmlock2nix)
-  # - nix-npm-buildpackage (https://github.com/serokell/nix-npm-buildpackage)
-  # - fetchNpmDeps
-  # - prefetch-npm-deps
-  #
-in
-  buildNpmPackage {
-    inherit pname version;
+  version = "v1.9.1-beta";
 
-    src = fetchFromGitHub {
-      owner = "hacksalot";
-      repo = pname;
-      rev = "ab6e7ee1a0f55608b531f4e644c298426291bb17";
-      hash = lib.fakeHash;
-    };
+  src = fetchFromGitHub {
+    owner = "hacksalot";
+    repo = pname;
+    rev = "ab6e7ee1a0f55608b531f4e644c298426291bb17";
+    hash = lib.fakeHash;
+  };
 
-    npmDepsHash =
-      lib.fakeHash; # "sha256-3GrHcwfu0rIPQ6e6SFa8Ve3VqL/KcEBRru9oAPAVGmo=";
-    dontNpmBuild = false;
+  npmDepsHash = lib.fakeHash; # "sha256-3GrHcwfu0rIPQ6e6SFa8Ve3VqL/KcEBRru9oAPAVGmo=";
+  dontNpmBuild = false;
 
-    meta = {
-      description = "HackMyResume - Resume toolkit & format conversion";
-      homepage = "https://github.com/hacksalot/${pname}";
-    };
-    #buildPhase = ''
-    #  echo "Building ${pname} ..."
-    #'';
-    #installPhase = ''
-    #  echo "Installing ${pname} ..."
-    #'';
-  }
+  meta = {
+    description = "HackMyResume - Resume toolkit & format conversion";
+    homepage = "https://fluentdesk.com/hackmyresume";
+    repo = "https://github.com/hacksalot/HackMyResume";
+    license = lib.licenses.mit;
+  };
+
+  # buildPhase = ''
+  #   echo "Building ${pname} ..."
+  # '';
+  # installPhase = ''
+  #   echo "Installing ${pname} ..."
+  # '';
+}
