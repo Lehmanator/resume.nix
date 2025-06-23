612e783d2f7a875b7ff43b8cf271f533aa72433d
Sam Lehman <lehmanator@noreply.codeberg.org>
2025-06-23 16:45:23 -0400

    fix(data.jsonresume.socials.signal): update to valid url

--- src/jsonresume/socials/signal.nix
+++ src/jsonresume/socials/signal.nix
@@ -1,8 +1,7 @@
 {...} @ args:
-rec
 {
   network = "Signal";
   username = "Lehmanator.01";
-  url = "https://signal.org/${username}";
+  url = "https://signal.me/#eu/nKYJgZClltLKPSGJQDvg6h3PyAhjs5c9Kxe4Nn2T2vad5-mzykVFEqvZnFTrI2fi";
 }
 // args
